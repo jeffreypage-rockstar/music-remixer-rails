467b16604709ee081977cbd6b9c484db55e33664
Lalit Kumar <lalit.maurya@newgenapps.com>
2015-09-04 12:06:17 +0530

    Updated unicorn conf file

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,9 @@ set :deploy_to, '/var/deploy/akashic'
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
+set :use_sudo, false
+set :bundle_binstubs, nil
+
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
