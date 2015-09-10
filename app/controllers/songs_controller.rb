047c77c0ad9bbc4626e916d5ffc4f4e263af8544
Lalit Kumar <lalit.maurya@newgenapps.com>
2015-09-10 11:38:03 +0530

    Redirect to configure page after song creation

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
 
     respond_to do |format|
       if @song.save
-        format.html { redirect_to root_path, notice: 'Song was successfully created.' }
+        format.html { redirect_to configure_song_path(@song), notice: 'Song was successfully created.' }
         # format.json { render :show, status: :created, location: @song }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class SongsController < ApplicationController
   def update
     respond_to do |format|
       if @song.update(song_params)
-        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Song was successfully updated.' }
         format.json { render :show, status: :ok, location: @song }
       else
         format.html { render :edit }
