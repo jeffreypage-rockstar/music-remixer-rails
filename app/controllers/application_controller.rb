9e248f04dc8a61fbdc040e90c22183d5c1202411
Mark Puckett <mark@8stem.com>
2015-11-14 08:43:46 -0800

    turn on basic auth'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # http_basic_authenticate_with name: "mix8", password: "8stem"
+  http_basic_authenticate_with name: "8stem", password: "mixer8"
 
 end
