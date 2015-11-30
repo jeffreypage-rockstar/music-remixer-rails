c734edf6389ca9187dc62316f81cb55c2dd45f22
Oleg Sokolov <olegsokolov513@hotmail.com>
2015-11-30 12:15:22 +0300

    fix clip file upload worker

--- app/workers/clip_file_upload_worker.rb
+++ app/workers/clip_file_upload_worker.rb
@@ -7,14 +7,14 @@ class ClipFileUploadWorker < ::CarrierWave::Workers::StoreAsset
     clip = constantized_resource.find id
     clip.update_attribute(:storing_status, :storing_done)
     song = clip.song
-    if !song.processing_done? && song.clips.where.not(storing_status: Clip.storing_statuses[:storing_done]).count == 0
-      song.update_attribute(:processing_status, :processing_done)
+    if !song.pending? && song.clips.where.not(storing_status: Clip.storing_statuses[:storing_done]).count == 0
+      song.update_attribute(:status, :pending)
     end
   end
 
   sidekiq_retries_exhausted do
     clip = constantized_resource.find id
     clip.update_attribute(:storing_status, :storing_failed)
-    clip.song.update_attribute(:processing_status, :processing_failed)
+    clip.song.update_attribute(:status, :failed)
   end
 end
