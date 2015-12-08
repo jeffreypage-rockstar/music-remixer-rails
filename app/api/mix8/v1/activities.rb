85ca71cfc29fa4a277aaa0bf7cf101014b2c83af
Oleg Sokolov <olegsokolov513@hotmail.com>
2015-12-08 12:12:39 +0300

    various filters for activities

--- app/api/mix8/v1/activities.rb
+++ app/api/mix8/v1/activities.rb
@@ -5,14 +5,21 @@ module Mix8
       include Grape::Kaminari
 
       resource :activities, desc: 'Activities' do
-        desc 'Get list of activities', { headers: { 'Authorization' => { description: 'Access Token', required: true } } }
+        desc 'Get list of activities', {headers: {'Authorization' => {description: 'Access Token', required: true}}}
         params do
           optional :since, type: DateTime, desc: 'since the specific datetime'
+          requires :filter, type: Symbol, values: [:all, :friends, :songs], default: :all, desc: 'filter'
         end
         paginate per_page: 20, max_per_page: 30, offset: 5
         get do
           authenticate!
           activities_query = PublicActivity::Activity.order('created_at DESC')
+          activities_query = case params[:filter]
+                               when :friends
+                                 activities_query.where(owner_id: current_user.followees.map(&:id))
+                               when :songs
+                                 activities_query.where(key: %w(song.create song.share song.like song.unlike))
+                             end
           activities_query = activities_query.where('created_at >= ?', params[:since]) if params[:since]
           activities = paginate(activities_query)
           present activities, with: Mix8::V1::Entities::Activity
