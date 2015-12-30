1fecbe879ca270894ff491b1cff67dd2388f7682
Oleg Sokolov <olegsokolov513@hotmail.com>
2015-12-30 12:45:23 +0300

    - configure ActionMailer to use SendGrid - create layout for mail templates

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure ActionMailer to Use SendGrid
+ActionMailer::Base.smtp_settings = {
+    :user_name => Rails.application.secrets.sendgrid_username,
+    :password => Rails.application.secrets.sendgrid_password,
+    :domain => 'mix8.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
