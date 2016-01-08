3dd968b89b0a216258866ae5759aa2c4bf0db225
Oleg Sokolov <olegsokolov513@hotmail.com>
2016-01-08 13:43:41 +0100

    tweak on unsubscribe and reset password page

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -13,34 +13,19 @@ class PasswordsController < Clearance::PasswordsController
   def update
     @user = find_user_for_update
 
-    @success = true
-    @message = ''
-    password_confirmation = params[:password_reset][:password_confirmation]
-    print password_confirmation
-    if password_confirmation == password_reset_params
-      if @user.update_password password_reset_params
-        respond_to do |format|
-          format.js
-        end
-      else
-        @success = false
-        flash_failure_after_update
-        render template: 'passwords/edit'
-      end
-    else
-      @success = false
-      @message = 'Passwords do not match'
+    if @user.update_password password_reset_params
       respond_to do |format|
         format.js
       end
+    else
+      flash_failure_after_update
+      render template: 'passwords/edit'
     end
-
   end
 
   def reset_password_success_modal
     render layout: 'modal'
   end
-
   def deliver_email(user)
     UserNotifier.reset_password_email(user).deliver_now
   end
