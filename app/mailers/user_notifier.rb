9b2c934d2d2ed6ab538a3adddebe7eb73e6dc7a7
Mark Puckett <mark@8stem.com>
2016-01-17 22:43:00 -0800

    beta user signup changes

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -2,8 +2,9 @@ class UserNotifier < ApplicationMailer
   default from: "Beta Team <beta@8stem.com>"
 
   # Send the beta migration email
-  def beta_migration_email(email)
-    mail(:to => email, :subject => '8Stem Beta Test')
+  def beta_migration_email(beta_user)
+    @beta_user = beta_user
+    mail(:to => "#{beta_user.name} <#{beta_user.email}>", :subject => '8Stem Beta Test')
   end
 
   # Send account verification email
