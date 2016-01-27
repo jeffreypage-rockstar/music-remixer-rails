299458577041686c647ef0b648c919870d52a951
Mark Puckett <mark@8stem.com>
2016-01-26 21:36:23 -0800

    rollbar deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,10 @@ set :rbenv_ruby, '2.2.3'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_path, '/home/deploy/.rbenv/'
 
+set :rollbar_token, Rails.application.secrets.rollbar_token
+set :rollbar_env, Proc.new { fetch :stage }
+set :rollbar_role, Proc.new { :app }
+
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/mix8'
 
