841e90af618bd6050e61721c0c2955245357c13f
Mark Puckett <mark@8stem.com>
2016-02-02 23:13:17 -0800

    fix fb connect, add soundcloud

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	# my mix8 - Dev
-	# provider :facebook, '853550408064609', '3e395df5bea7696742cb192c60848f53'
-
-	# 8stem - Dev
-	provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret, provider_ignores_state: true
-  provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret, provider_ignores_state: true
-
-	# from Adam
-	# provider :facebook, '1519009081750998', '45c00ac7fe9f421588d9fdffe07d24b1'
-
-	# TOOD for production
-	# provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'public_profile', info_fields: 'id,name,link'
+  provider :facebook,   Rails.application.secrets.facebook_key,   Rails.application.secrets.facebook_secret, provider_ignores_state: true
+  provider :twitter,    Rails.application.secrets.twitter_key,    Rails.application.secrets.twitter_secret, provider_ignores_state: true
+  provider :soundcloud, Rails.application.secrets.soundcloud_key, Rails.application.secrets.soundcloud_secret, provider_ignores_state: true
 end
