ff9979d9deb0fe008e314412ff17f2f2b3f8b4a5
Mark Puckett <mark@8stem.com>
2016-02-07 22:28:38 -0800

    email preview and email template hacks

--- test/mailers/previews/user_notifier_preview.rb
+++ test/mailers/previews/user_notifier_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_notifier
 class UserNotifierPreview < ActionMailer::Preview
 
+  def account_verification_email
+    UserNotifier.account_verification_email(User.first)
+  end
+
 end
