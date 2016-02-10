53aae6154e032d6977866c4a0423edbdd1b87b12
Mark Puckett <mark@8stem.com>
2016-02-09 16:08:08 -0800

    fix mix8.dev auth

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@
 Rails.application.config.session_store :cookie_store, {
   :key => '_8stem_session',
   :expire_after => 90.days,
-  :domain => Rails.env.production? ? '.8stem.com' : (Rails.env.staging? ? '.mix8.com' : '.8stem.dev'),
+  :domain => Rails.env.production? ? '.8stem.com' : (Rails.env.staging? ? '.mix8.com' : '.mix8.dev'),
   :tld_length => 2
 }
 
