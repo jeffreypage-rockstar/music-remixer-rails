2436458600c23ecdb5ddd6af4cf5508417d09c09
Mark Puckett <mark@8stem.com>
2016-02-16 23:08:28 -0800

    logging

--- app/workers/song_zipfile_upload_worker.rb
+++ app/workers/song_zipfile_upload_worker.rb
@@ -3,14 +3,16 @@ class SongZipfileUploadWorker < ::CarrierWave::Workers::StoreAsset
 
   def perform(*args)
     set_args(*args)
+    # do some stuff first
     song = constantized_resource.find id
     song.extract_zipfile!
     song.build_parts_and_clips
-
+    # now have part class do the upload
     super(*args)
   end
 
   sidekiq_retries_exhausted do
+    logger.error "SongZipfileUploadWorker sidekiq_retries_exhausted for song #{id}, setting status to failed"
     song = constantized_resource.find id
     song.update_attribute(:status, :failed)
   end
