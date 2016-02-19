1c814a91395f6f2f7feb00fb78a956608dfe8316
Mark Puckett <mark@8stem.com>
2016-02-18 18:06:58 -0800

    no more following yourself

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,9 +1,9 @@
 class UserPolicy < ApplicationPolicy
   def follow?
-    not record.nil? or (user.follows?(record) or user.id == record.id)
+    user.nil? or (!user.follows?(record) and user.id != record.id)
   end
 
   def unfollow?
-    !record.nil? and !user.nil? and user.follows?(record)
+    !user.nil? and user.follows?(record)
   end
 end
