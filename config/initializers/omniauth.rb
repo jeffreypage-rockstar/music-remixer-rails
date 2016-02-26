2815464a1f1f0277a511ea50acdfcfa295a35bdc
Mark Puckett <mark@8stem.com>
2016-02-25 17:08:28 -0800

    get email from fb auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,   Rails.application.secrets.facebook_key,   Rails.application.secrets.facebook_secret, provider_ignores_state: true
+  provider :facebook,   Rails.application.secrets.facebook_key,   Rails.application.secrets.facebook_secret,
+           :scope => 'email,public_profile',
+           :info_fields => 'email,name',
+           :display => 'popup'
   provider :twitter,    Rails.application.secrets.twitter_key,    Rails.application.secrets.twitter_secret, provider_ignores_state: true
   provider :soundcloud, Rails.application.secrets.soundcloud_key, Rails.application.secrets.soundcloud_secret, provider_ignores_state: true
 end
