c0213761a77ac1fd89fcbf0abe7c72ac692a17ab
Mark Puckett <mark@8stem.com>
2016-03-19 00:38:30 -0700

    safety checks for beta import

--- db/import_beta_users.rb
+++ db/import_beta_users.rb
@@ -5,5 +5,17 @@ text = File.read(File.dirname(__FILE__) + '/beta-users-test.csv')
 csv = CSV.parse(text, :headers => true)
 csv.each do |row|
   row = row.to_hash.with_indifferent_access
-  Referral.create!(row.to_hash.symbolize_keys)
+  row = row.to_hash.symbolize_keys
+
+  email = row[:email]
+  if user = BetaUser.find_by_email(row[:email])
+    puts "BetaUser already exists for #{row[:email]}"
+  elsif user = User.find_by_email(row[:email])
+    puts "BetaUser does NOT exist, but User already exists for #{row[:email]}"
+  elsif referral = Referral.find_by_email(row[:email])
+    puts "Referral already exists for #{row[:email]}"
+  else
+    puts "Creating new beta user referral for #{row[:email]}"
+    Referral.create!(row)
+  end
 end
